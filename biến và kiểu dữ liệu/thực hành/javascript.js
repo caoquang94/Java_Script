5c241811249daf20c507bd4ac1975370a482e2ef
caoquang94 <caoquang811994@gmail.com>
2019-10-10 09:52:45 +0700

    .

---
+++ biến và kiểu dữ liệu/thực hành/javascript.js
@@ -0,0 +1,6 @@
+
+let chieudai = parseInt(prompt('nhập chiều dài'));
+let chieurong = parseInt(prompt('nhập chiều rộng'));
+
+let dientich = chieudai *chieurong;
+document.write('diện tích = ' +dientich);
